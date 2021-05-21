ebc5b7abd8b82c79636f11cc28972358ba6e7b97
Kevin Köllmann <mail@kevinkoellmann.de>
2021-05-21 13:44:42 +0200

    ask-passphrase option now in reprepro options file instead of add-script

--- add_debian_package_to_git_repository.sh
+++ add_debian_package_to_git_repository.sh
@@ -4,5 +4,4 @@ set -e
 
 ./build_debian_package.sh
 vagrant up
-vagrant ssh -c 'cd project/reprepro && reprepro --ask-passphrase includedeb general ../target/icinga2-zulip-notifications_*_all.deb'
-
+vagrant ssh -c 'cd project/reprepro && reprepro includedeb general ../target/icinga2-zulip-notifications_*_all.deb'
